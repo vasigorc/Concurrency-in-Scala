c31570005883ed1bd2c02c18551e776e93dff68c
Vasile Gorcinschi <vasigorc@gmail.com>
2018-06-01 00:21:13 -0400

    Exercises.matcher (started)

--- chapter01_introduction/src/main/scala/org/learningconcurrency/Exercises.scala
+++ chapter01_introduction/src/main/scala/org/learningconcurrency/Exercises.scala
@@ -12,10 +12,10 @@ object Exercises {
   }
 
   def check[T](xs: Seq[T])(pred: T => Boolean): Boolean = { //ex 3
-    Try{xs forall(pred)} getOrElse(false)
+    Try{xs forall pred} getOrElse false
   }
 
-  case class Pair[P,Q](val first: P, val second: Q) //ex 4
+  case class Pair[P,Q](first: P, second: Q) //ex 4
 
   def permutations(x: String): Seq[String] = x.permutations.toSeq //ex 5
 
@@ -34,7 +34,7 @@ object Exercises {
 
   def combinations(n: Int, xs: Seq[Int]):Iterator[Seq[Int]] = xs.combinations(n)  // ex 6
 
-  def combinationsByHand(n: Int, xs: Seq[Int]):Iterator[Seq[Int]] = {
+  def combinationsByHand(n: Int, xs: Seq[Int]):Iterator[Seq[Int]] = { // ex 6
     n match {
       case 0 => Iterator()
       case 1 => Iterator(xs)
@@ -43,5 +43,11 @@ object Exercises {
         lesser.flatMap(seq=> lesser.map(seq1=>seq1++seq))
       }
     }
+  }
+
+  def matcher(regex: String):PartialFunction[String, List[String]] = new PartialFunction[String, List[String]] { // ex 7
+    override def isDefinedAt(x: String): Boolean = !apply(x).isEmpty
+
+    override def apply(v1: String): List[String] = ???
   }
 }
