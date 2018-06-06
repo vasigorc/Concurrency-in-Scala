77cdc93fbb52039b22b9a0d911981eeb3d74d0db
Vasile Gorcinschi <vasigorc@gmail.com>
2018-06-06 17:54:16 -0400

    chapter_02_concurrency_on_jvm: Ex 2 & 3

--- chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/Exercises02.scala
+++ chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/Exercises02.scala
@@ -16,7 +16,47 @@ object Exercises02 {
       log(s"value $bholder calculated")
     }
 
-    t1.join(); t2.join();
+    t1.join(); t2.join()
     (aholder, bholder)
+  }
+
+  def periodically(duration: Long)(b: =>Unit):Unit ={ // ex 2
+  //graceful shutdown
+  object Worker extends Thread{
+    var terminated = false
+
+    def repeat():Option[() => Unit] = {
+      Thread.sleep(duration)
+      if(!terminated) Some(() =>b) else None
+    }
+
+    override def run(): Unit = repeat() match {
+      case Some(action) => action; run()
+      case None =>
+    }
+  }
+    Worker.start()
+    Thread.sleep(duration*10)//after 10*times duration
+    Worker.terminated = true
+  }
+
+  class SyncVar[T]{ // ex 3
+    var t: T = null.asInstanceOf[T]
+
+    def get():T = this.synchronized{
+      if(t != null) {
+        val temp = t
+        t = null.asInstanceOf[T]
+        temp
+      } else throw new Exception("variable not initialized")
+    }
+
+    def put(x: T):Unit = this.synchronized{
+      if(t == null){
+        t = x
+      } else {
+        throw new Exception("variable not consumed yet")
+      }
+    }
   }
 }
--- chapter02_concurrency_on_jvm/src/test/scala/org/learningconcurrency/Exercises02Spec.scala
+++ chapter02_concurrency_on_jvm/src/test/scala/org/learningconcurrency/Exercises02Spec.scala
@@ -9,4 +9,15 @@ class Exercises02Spec extends BaseSpec{
     val bynameB = math.random()
     parallel(bynameA, bynameB) should equal((bynameA, bynameB))
   }
+
+  "sequential calls to a SyncVar's put() and get()" should "return initial value" in {
+    var sync = new SyncVar[Int]
+
+    val t1 = thread {
+      sync.put(10)
+    }
+
+    t1.join()
+    sync.get() should equal(10)
+  }
 }
