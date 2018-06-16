495e7b414984cac4912cba7b6d32af19bf7c2c52
Vasile Gorcinschi <vasigorc@gmail.com>
2018-06-16 18:36:58 -0400

    chapter_02_concurrency_on_jvm: Ex 10

--- chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/concurrency_on_jvm/PriorityTaskPool_Ex10.scala
+++ chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/concurrency_on_jvm/PriorityTaskPool_Ex10.scala
@@ -2,19 +2,21 @@ package org.learningconcurrency.concurrency_on_jvm
 
 import java.util.concurrent.atomic.AtomicBoolean
 
-class PriorityTaskPool_Ex10(p: Int, important: Int) extends PriorityTaskPool_Ex9(p) {
+class PriorityTaskPool_Ex10(p: Int, important: Int) extends PriorityTaskPool {
 
-  private var poolClosed: AtomicBoolean = AtomicBoolean
+  private var poolClosed: AtomicBoolean = new AtomicBoolean(false)
+
+  (1 to p).map((i) => new AttentiveWorker()).map(_.start)
 
   private class AttentiveWorker extends Worker {
 
-    override def poll(): Option[Task] = tasks.synchronized {
+    def pollOpt(): Option[Task] = tasks.synchronized {
       while (tasks.isEmpty) tasks.wait()
       if(!poolClosed.get() || tasks.peek.priority > important) Some(tasks.dequeue()) else None
     }
 
-    override def run(): Unit = poll() match {
-      case Some(task) => task(); run()
+    override def run(): Unit = pollOpt() match {
+      case Some(task) => task.task(); run()
       case None =>
     }
   }
@@ -22,4 +24,20 @@ class PriorityTaskPool_Ex10(p: Int, important: Int) extends PriorityTaskPool_Ex9
   def shutdown(): Unit = {
     poolClosed.getAndSet(true)
   }
+}
+
+object PriorityTaskPool_Ex10 extends App{
+
+  import org.learningconcurrency._
+
+  def apply(p: Int, important: Int): PriorityTaskPool_Ex10 = new PriorityTaskPool_Ex10(p, important)
+
+  private val pool_Ex = PriorityTaskPool_Ex10(4, 30)
+
+  (1 to 60).foreach((i) => {
+    val a = (Math.random*1000).toInt
+    pool_Ex.asynchronous(60-a){()=>log(s"<- $a")}
+  })
+  pool_Ex.shutdown()
+  Thread.sleep(5000)
 }
