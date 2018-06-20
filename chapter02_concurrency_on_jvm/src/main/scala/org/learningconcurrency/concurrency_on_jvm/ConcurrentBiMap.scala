d0ccdcb4032c5c2b8e050c4f098b0e6b6cd9a68f
Vasile Gorcinschi <vasigorc@gmail.com>
2018-06-20 18:17:44 -0400

    chapter_02_concurrency_on_jvm: continued Ex11 (ConcurrentBiMap)

--- chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/concurrency_on_jvm/ConcurrentBiMap.scala
+++ chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/concurrency_on_jvm/ConcurrentBiMap.scala
@@ -1,5 +1,7 @@
 package org.learningconcurrency.concurrency_on_jvm
 
+import java.util.Objects
+
 import scala.collection.mutable.{HashMap => MyMap}
 
 // for exercises 11 and 12
@@ -7,5 +9,41 @@ class ConcurrentBiMap [K, V] {
   private val byKeys = new MyMap[K, V]
   private val byValues = new MyMap[V, K]
 
-  def put(k: K, v: V):Option[(K, V)] = ???
+  protected def checkKey(k: K): Boolean = k != null
+
+  protected def checkValue(v: V): Boolean = v != null
+
+  def containsKey(k: K): Boolean = byKeys.contains(k)
+
+  def getValue(k: K): Option[V] = byKeys.get(k)
+
+  // should return previous value
+  def put(k: K, v: V):Option[(K, V)] = {
+    //java's concurrent hash map doesn't allow null keys or values
+    if(checkKey(k) && checkValue(v)){
+      //if key is present and value is the same, nothing to do here
+      if(containsKey(k) && Objects.equals(v, getValue(k).get)){
+        Some(k, v)
+      }
+      //should be safe to do get since our map doesn't contain null keys or values
+      val oldKvPair: (K, V) = byKeys.get(k).map(oldValue=> (k, oldValue)).get
+      byKeys.put(k, v)
+      byValues.put(v, k)
+      Some(oldKvPair)
+    } else None
+  }
+
+  def removeKey(k: K): Option[V] = {
+    byKeys.remove(k).map(value => {
+      byValues.remove(value)
+      value
+    })
+  }
+
+  def removeValue(v: V):Option[K] = {
+    byValues.remove(v).map(key => {
+      byKeys.remove(key)
+      key
+    })
+  }
 }
