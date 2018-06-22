6d5f82639710a82572c500c4947c3bc20d1db6e0
Vasile Gorcinschi <vasigorc@gmail.com>
2018-06-21 21:15:25 -0400

    chapter_02_concurrency_on_jvm: ConcurrentBiMap - completed, inserting and replacing 100 000 tuples takes 49 secs

--- chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/concurrency_on_jvm/ConcurrentBiMap.scala
+++ chapter02_concurrency_on_jvm/src/main/scala/org/learningconcurrency/concurrency_on_jvm/ConcurrentBiMap.scala
@@ -59,9 +59,11 @@ class ConcurrentBiMap [K, V] {
   def iterator: Iterator[(K, V)] = byKeys.iterator
 
   def replace(k1: K, v1: V, k2: K, v2: V): Unit = {
-    if(containsKey(k1) && Objects.equals(v1, getValue(k1).get)){
-      removeKey(k1)
-      put(k2, v2)
-    }
+    getValue(k1).map(v =>{
+      if(Objects.equals(v, v1)){
+        removeKey(k1)
+        put(k2, v2)
+      }
+    })
   }
 }
