584a7d3cb945cd75f75ef0f8a9a71eb1e84a63e4
Vasile Gorcinschi <vasigorc@gmail.com>
2018-06-30 08:46:02 -0400

    First Actor project

--- chapter01_introduction/src/main/scala/org/learningconcurrency/introduction/ColleaguesManager.scala
+++ chapter01_introduction/src/main/scala/org/learningconcurrency/introduction/ColleaguesManager.scala
@@ -1,12 +1,23 @@
 package org.learningconcurrency.introduction
 
-import akka.actor.{Actor, ActorLogging, SupervisorStrategy}
+import akka.actor.{Actor, ActorLogging, PoisonPill, SupervisorStrategy}
+
+import scala.collection.immutable
 
 class ColleaguesManager extends Actor with ActorLogging{
+  import Colleague._
 
-  override def preStart(): Unit = ???
+  override def preStart(): Unit = {
+    val colleagues: immutable.Seq[String] = (1 to 4).map("colleague_"+_)
+    colleagues.foreach(c => props(colleagues.diff(Seq(c))))
+  }
 
   override def supervisorStrategy: SupervisorStrategy = super.supervisorStrategy
 
-  override def receive: Receive = ???
+  override def receive: Receive = {
+    case GameOver(time, attenders) =>
+      log.info("{} agreed to meet at {} in a local bar", attenders.mkString(", "), time)
+      context.children.foreach(_ ! PoisonPill)
+      context.system.terminate()
+  }
 }
