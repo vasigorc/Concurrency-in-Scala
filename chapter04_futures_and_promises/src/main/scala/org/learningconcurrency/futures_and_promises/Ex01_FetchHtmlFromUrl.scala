d46961a56f43b89e24d955422e02e2e8c44d4b7d
Vasile Gorcinschi <vasile.gorcinschi@workjam.com>
2018-12-29 12:13:13 -0500

    Started Ex01

---
+++ chapter04_futures_and_promises/src/main/scala/org/learningconcurrency/futures_and_promises/Ex01_FetchHtmlFromUrl.scala
@@ -0,0 +1,14 @@
+package org.learningconcurrency.futures_and_promises
+
+/**
+  * Exercise 1 Implement a command-line program that asks the user to input
+  * a URL of some website, and displays the HTML of that website. Between the
+  * time that the user hits ENTER and the time that the HTML is retrieved, the
+  * program should repetitively print a . to the standard output every 50 milliseconds
+  * , with a 2 second timeout. Use only futures and promises, and avoid synchronization
+  * primitives from the previous chapters. You may reuse the timeout method defined
+  * in this chapter.
+  */
+object Ex01_FetchHtmlFromUrl extends App {
+
+}
