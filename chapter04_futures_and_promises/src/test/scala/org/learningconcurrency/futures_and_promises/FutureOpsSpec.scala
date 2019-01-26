c087b4ff166e535ccf50744a79d340b89dc173dd
Vasile Gorcinschi <vasigorc@gmail.com>
2019-01-26 04:58:30 -0500

    Added test coverage for FutureOps.exists

---
+++ chapter04_futures_and_promises/src/test/scala/org/learningconcurrency/futures_and_promises/FutureOpsSpec.scala
@@ -0,0 +1,27 @@
+package org.learningconcurrency.futures_and_promises
+
+import org.scalatest.AsyncFunSpec
+
+import scala.concurrent.Future
+
+class FutureOpsSpec extends AsyncFunSpec {
+
+  val isEven: Int => Boolean = i => i %2 == 0
+
+  describe("exists") {
+    it("will yield false when upstream future throws an exception") {
+      val failedFuture: Future[Int] = Future.failed(new RuntimeException)
+      failedFuture exists(isEven) map(result => assert(!result))
+    }
+
+    it("will yield true when the provided predicate evaluate") {
+      val successfulFuture = Future.successful(10)
+      successfulFuture exists(isEven) map(assert(_))
+    }
+
+    it("will yield false when the provided predicate doesn't evaluate") {
+      val unsuccessfulFuture = Future.successful(7)
+      unsuccessfulFuture exists(isEven) map(result => assert(!result))
+    }
+  }
+}
